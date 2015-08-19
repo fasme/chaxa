4ab27ec4e3751a92d28e105b0ead67c8a916b02d
Dell Inspiron <fasme2h@gmail.com>
2015-08-19 14:40:15 -0300

    o

--- app/views/portal/layouts.blade.php
+++ app/views/portal/layouts.blade.php
@@ -90,7 +90,7 @@
                                 <li><a href="shortcodes.html">Shortcodes</a></li>
                             </ul>
                         </li>-->
-                        @if((Auth::user()->perfil == "admin") || (Auth::user()->perfil == "adminprevencion") || (Auth::user()->perfil == "adminmantencion") || (Auth::user()->perfil == "admingerente") || (Auth::user()->perfil == "adminfacturacion") || (Auth::user()->perfil == "adminrhh"))
+                        @if((Auth::user()->perfil == "admin") || (Auth::user()->perfil == "adminprevencion") || (Auth::user()->perfil == "adminmantencion") || (Auth::user()->perfil == "admingerente") || (Auth::user()->perfil == "adminfacturacion") || (Auth::user()->perfil == "adminrrhh"))
                          <li><a href="{{URL::to('/')}}">Administracion</a></li> 
                         @endif
                         @if (Auth::check())
