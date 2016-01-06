88a851775d9a64a9382e0af9b758c5e406e5c5b1
Dell Inspiron <fasme2h@gmail.com>
2016-01-06 20:02:46 -0300

    i

--- app/controllers/MatrizController.php
+++ app/controllers/MatrizController.php
@@ -229,7 +229,8 @@ return Redirect::to('matriz/update/'.$id)->withInput()->withErrors($matriz->erro
 
         $matrizs = Matriz::all();
         $view = View::make('matriz.pdf')->with("matrizs",$matrizs);
-        return PDF::load($view, 'tabloid', 'landscape')->show();
+        return $view;
+        //return PDF::load($view, 'tabloid', 'landscape')->show();
 
     }
 
